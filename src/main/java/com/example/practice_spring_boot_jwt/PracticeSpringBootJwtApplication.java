a6bbdad09a9070f0596cedcef6febfa2c27651c7
trungtashi99 <trungtashi@gmai.com>
2022-11-07 15:27:02 +0700

    Practice spring boot jwt

---
+++ src/main/java/com/example/practice_spring_boot_jwt/PracticeSpringBootJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.practice_spring_boot_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringBootJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringBootJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/practice_spring_boot_jwt/PracticeSpringBootJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice_spring_boot_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringBootJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
